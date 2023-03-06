0634416898a5657efc363c92e6da159b2986fa80
TatsuyaMaeta <tat.mae084@gmail.com>
2023-03-06 18:01:23 +0900

    add context

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -501,7 +501,7 @@
                 </div>
 
                 <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    githubからpullしてきた Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </div>
             </div>
         </div>
